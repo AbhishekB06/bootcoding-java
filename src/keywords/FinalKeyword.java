bc49accfb2c531f2970d41ef3695f1c2d8922b15
AbhishekB06 <abhishekab852000@gmail.com>
2023-01-12 23:55:26 +0530

    Final keyword program!

---
+++ src/keywords/FinalKeyword.java
@@ -0,0 +1,15 @@
+package keywords;
+
+import oops.Marker;
+
+public class FinalKeyword {
+    public static void main(String[] args) {
+        final int a = 10;
+
+        final double Pi = 3.14;
+
+        final Marker blueMarker =new Marker();
+        blueMarker.color = "Blue";
+        blueMarker.color = "Red";
+    }
+}
