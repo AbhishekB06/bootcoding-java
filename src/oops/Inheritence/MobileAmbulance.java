2b71cae139e9a697cd12319a0b231866cab86c42
AbhishekB06 <abhishekab852000@gmail.com>
2023-01-14 22:12:20 +0530

    MethodApi

---
+++ src/oops/Inheritence/MobileAmbulance.java
@@ -0,0 +1,9 @@
+package oops.Inheritence;
+
+import oops.Encapsulation.Ambulance;
+
+public class MobileAmbulance extends Ambulance {
+    public void assignCharges(){
+        charges = 100;
+    }
+}
