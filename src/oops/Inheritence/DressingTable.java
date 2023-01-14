4aea433ab9dde5c2658df90197a4f0ba188d9b6e
AbhishekB06 <abhishekab852000@gmail.com>
2023-01-14 22:11:43 +0530

    MethodApi

---
+++ src/oops/Inheritence/DressingTable.java
@@ -0,0 +1,10 @@
+package oops.Inheritence;
+
+import oops.RaceCycle;
+
+public class DressingTable extends Table{
+    public static void main(String[] args) {
+        RaceCycle rc = new RaceCycle();
+
+    }
+}
