f44a1928b6571946e82de23ea3b2ce4cf1823d62
AbhishekB06 <abhishekab852000@gmail.com>
2023-01-29 20:43:41 +0530

    Program to display the pattern like right angle triangle with a number

--- src/com/bootcoding/java/Patterns/RightAngleTriangleNumber.java
+++ src/com/bootcoding/java/Patterns/RightAngleTriangleNumber.java
@@ -5,7 +5,7 @@ public class RightAngleTriangleNumber {
         int n = 5;
         for(int i = 0; i <= n; i++){
             for(int j = 1; j <= i; j++){
-                System.out.print( j + " ");
+                System.out.print( j );
             }
             System.out.println();
         }
