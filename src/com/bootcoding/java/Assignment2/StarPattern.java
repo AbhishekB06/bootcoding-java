8090a48849dba228b41c9523fe9aa9df31b254ae
AbhishekB06 <abhishekab852000@gmail.com>
2023-03-23 19:56:45 +0530

    example of star pattern

---
+++ src/com/bootcoding/java/Assignment2/StarPattern.java
@@ -0,0 +1,12 @@
+package com.bootcoding.java.Assignment2;
+
+public class StarPattern {
+    public static void main(String[] args) {
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 5; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
