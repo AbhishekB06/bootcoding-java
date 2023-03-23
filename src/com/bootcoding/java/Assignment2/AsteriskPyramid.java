8347108e6b6dbee260958891ad7fcf969d3a9f0f
AbhishekB06 <abhishekab852000@gmail.com>
2023-03-23 22:59:00 +0530

    example of star pattern reverse right angled triangle

---
+++ src/com/bootcoding/java/Assignment2/AsteriskPyramid.java
@@ -0,0 +1,15 @@
+package com.bootcoding.java.Assignment2;
+
+public class AsteriskPyramid {
+    public static void main(String[] args) {
+        for (int i=0; i<5; i++){
+            for (int j=4-i; j>0; j--){
+                System.out.print(" ");
+            }
+            for(int j=0; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
